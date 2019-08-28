be44a3401572a809bda34ab514e10574c42d0cd1
Ethan <weisfeldethan@hotmail.com>
2019-08-28 02:45:10 -0400

    made some more code

---
+++ explanations.js
@@ -0,0 +1,35 @@
+// Explanations 
+var favColor = 'red'; // String
+var myFavColors = ['blue', 'red', 'green']; // Array
+var numofFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 103;
+	isMale: true
+
+};
+
+var WhatIsMyFavColor = function () {
+	return false;
+
+	return true;
+
+	return 'blue'
+
+}
+
+var DoSomething = function () {
+	console.log("DoSomething");
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullname = function (firstname, lastname) {
+	return firstname + " " +lastname;
+}
+
---
+++ explanationstwo.js
@@ -0,0 +1,11 @@
+// Explanation Number Two
+
+var number1 = 10;
+var number2 = 100;
+
+
+if (number1 > number2) {
+	alert('true');
+} else {
+	alert('false');
+}
